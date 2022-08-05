2096a24023d595776f4862615fe68094c07b3f3c
Saikat7827 <saikatchakraborty957.sc@gmail.com>
2022-08-06 00:55:24 +0530

    Added: random dice 1

---
+++ 1dice.sh
@@ -0,0 +1,18 @@
+dice_num=$(( 1 + RANDOM % 6))
+
+echo "Dice :" $dice_num
+
+case $dice_num in
+1) echo "one"
+   ;;
+2) echo "two"
+   ;;
+3) echo "three"
+   ;;
+4) echo "four"
+   ;;
+5) echo "five"
+   ;;
+*) echo "six"
+
+esac
